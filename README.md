b2c4f27c05d4f32a096001b43cfcbe30ed2f80ed
Mohamed Oulma <mohamed.oulma96@gmail.com>
2025-12-16 10:03:26 +0100

    fix issue

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ open up localhost:
 
 	
 ## 3. Create ECR repo to store/save docker image
-    - Save the URI: 315865595366.dkr.ecr.us-east-1.amazonaws.com/medicalbot
+    - Save the URI: 245713704990.dkr.ecr.eu-north-1.amazonaws.com/raqababot
 
 	
 ## 4. Create EC2 machine (Ubuntu) 
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical_chatbot",
+    name="raqaba_ai_chatbot",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="oulma mohamed",
+    author_email="mohamed.oulma96@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
